7119793bb8c455055a63eb4aa0c24638521f646e
Abhay Sharma <74869287+codeforgrow@users.noreply.github.com>
2024-02-29 15:55:20 +0530

    🚀 : Setup ShipShop Project Structure - added the redux tollKit - uses the tailwind Css styling Components - setup the initial routing - create the Product list page, login page and Sign up page

---
+++ src/features/product-list/ProductListApi.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProductListApi = () => {
+  return (
+    <div>ProductListApi</div>
+  )
+}
+
+export default ProductListApi
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
@@ -5,6 +6,8 @@ import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
+
+
 
 const container = document.getElementById('root');
 const root = createRoot(container);
